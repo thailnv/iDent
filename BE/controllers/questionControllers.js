2bb9437c447b052e6dda1e0235943cdfaa69c5dd
thailnv <56252864+thailnv@users.noreply.github.com>
2021-04-22 12:37:48 +0700

    remove node_modules

--- BE/controllers/questionControllers.js
+++
@@ -1,7 +0,0 @@
-const base = require('./baseController');
-const { Question } = require('../models/questionModel');
-
-exports.getOne = base.getOne(Question);
-exports.getAll = base.getAll(Question);
-exports.addOne = base.addOne(Question);
-exports.updateOme = base.updateOne(Question);
