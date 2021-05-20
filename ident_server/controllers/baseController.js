a84e2d7de4a97ea040dc26230fe8dad49aaac200
thailnv <56252864+thailnv@users.noreply.github.com>
2021-05-20 22:20:05 +0700

    change db to mongodb atlas

--- ident_server/controllers/baseController.js
+++ ident_server/controllers/baseController.js
@@ -1,18 +1,19 @@
 exports.deleteOne = (Model) => async (req, res, next) => {
   try {
     const doc = await Model.findByIdAndDelete(req.params.id);
-
     if (!doc) {
-      //!
       res.status(404).send("No document found with that id");
       return;
     }
-
     res.status(204).json({
       status: "success",
       data: null,
     });
   } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong please try again latter",
+    });
     next(error);
   }
 };
@@ -23,32 +24,34 @@ exports.addOne = (Model) => async (req, res, next) => {
 
     res.status(201).json({
       status: "success",
-      data: {
-        doc,
-      },
+      data: doc,
     });
-  } catch (err) {
-    res.status(400);
-    next(err);
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong please try again latter",
+    });
+    next(error);
   }
 };
 
 exports.updateOne = (Model) => async (req, res, next) => {
   try {
     const doc = await Model.findByIdAndUpdate(req.params.id, req.body);
-
     if (!doc) {
-      //!
       res.status(404).send("No document found with that id");
       return;
     }
-
     res.status(200).json({
       status: "success",
       data: doc,
     });
   } catch (error) {
     next(error);
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong please try again latter!",
+    });
   }
 };
 
@@ -61,12 +64,14 @@ exports.getOnePopulate = (Model, populate) => async (req, res, next) => {
       });
     } else {
       res.status(400).json({
-        status: "Can find any doc with that id!",
+        status: "fail",
+        message: "No document found with that id!",
       });
     }
   } catch (error) {
-    res.status(400).json({
-      error: "Server can handle this request right now!",
+    res.status(500).json({
+      status: "fail",
+      message: "",
     });
     next(error);
   }
@@ -75,18 +80,22 @@ exports.getOnePopulate = (Model, populate) => async (req, res, next) => {
 exports.getOne = (Model) => async (req, res, next) => {
   try {
     const doc = await Model.findById(req.params.id).lean();
-
     if (!doc) {
-      //!
-      res.status(404).send("No document found with that id");
+      res.status(404).json({
+        status: "fail",
+        message: "No document found with that id!",
+      });
       return;
     }
-
     res.status(200).json({
       status: "success",
       data: doc,
     });
   } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong please try again latter!",
+    });
     next(error);
   }
 };
@@ -100,12 +109,14 @@ exports.getAllPopulate = (Model, populate) => async (req, res, next) => {
       });
     } else {
       res.status(400).json({
-        status: "Can not find!",
+        status: "fail",
+        message: "No document found with that id!",
       });
     }
   } catch (error) {
-    res.status(400).json({
-      error: "Server can handle this request right now!",
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong please try again latter!",
     });
     next(error);
   }
@@ -114,18 +125,22 @@ exports.getAllPopulate = (Model, populate) => async (req, res, next) => {
 exports.getAll = (Model) => async (req, res, next) => {
   try {
     const docs = await Model.find();
-
     if (!docs) {
-      //!
-      res.status(404).send("No document found");
+      res.status(404).json({
+        status: "fail",
+        message: "No document found with that id!",
+      });
       return;
     }
-
     res.status(200).json({
       status: "success",
       data: docs,
     });
   } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong please try again latter!",
+    });
     next(error);
   }
 };
